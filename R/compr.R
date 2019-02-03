19d8751f81643afcb7e8b19fa772fe342ba96a80
Joshua <Joshua.Halls@ons.gov.uk>
2019-02-03 12:13:57 +0000

    package desc not working

---
+++ R/compr.R
@@ -0,0 +1,9 @@
+#'@details
+#' This package is used for caculating compliance costs.
+#' First, the function compliance_data is used to read the data in and convert to the requesite class.
+#'
+#' @keywords internal
+#' "_PACKAGE"
+#'
+#' @references
+#' https://gss.civilservice.gov.uk/wp-content/uploads/2015/12/Guidance-on-Calculating-Compliance-Costs.pdf
